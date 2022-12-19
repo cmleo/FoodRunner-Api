8680b64611da0511c977cb2d93f32a1b7e6b131c
cmleo <leo.marinel13@yahoo.com>
2022-12-19 17:52:59 +0200

    creating some test routes for products

--- Api/app.js
+++ Api/app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-	res.status(200).json({
-		message: 'It Works!',
-	});
-});
+const productRoutes = require('../Api/Routes/products');
+
+app.use('/products', productRoutes);
 
 module.exports = app;
