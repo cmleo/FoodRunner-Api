0895fae8399b130606e56ef1035380b911786f46
cmleo <leo.marinel13@yahoo.com>
2022-12-20 17:34:43 +0200

    added user middleware

--- Api/app.js
+++ Api/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const productRoutes = require('../Api/Routes/products');
+const productRoutes = require('./Routes/products');
+const userRoutes = require('./Routes/users');
 
-//Middlewares
+// ExpressMiddlewares
 app.use(express.json());
 app.use(express.urlencoded());
 
+// AppMiddlewares
 app.use('/products', productRoutes);
+app.use('/user', userRoutes);
 
 mongoose.connect('mongodb+srv://cmleo:cmleo123@cluster0.fmaydcc.mongodb.net/?retryWrites=true&w=majority', {
 	useNewUrlParser: true,
