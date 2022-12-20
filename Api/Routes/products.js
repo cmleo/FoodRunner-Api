1488e1ba7e15097c8ef2ae032cdb9d7877b32605
cmleo <leo.marinel13@yahoo.com>
2022-12-20 15:28:42 +0200

    updated the get and post routes for products

--- Api/Routes/products.js
+++ Api/Routes/products.js
@@ -1,28 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const Product = require('../Models/Product');
 
 // Get all products
-router.get('/', (req, res) => {
-	Product.find((err, products) => {
-		try {
-			res.json(products);
-		} catch (err) {
-			res.send(err);
-		}
-	});
+router.get('/', (req, res, next) => {
+	Product.find()
+		.exec()
+		.then((docs) => {
+			res.status(200).json(docs);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				error: err,
+			});
+		});
 });
 
 // Create a new product
-router.post('/', (req, res) => {
-	const newProduct = new Product(req.body);
-	newProduct.save((err, product) => {
-		try {
-			res.json(product);
-		} catch (err) {
-			res.send(err);
-		}
+router.post('/', (req, res, next) => {
+	const product = new Product({
+		_id: new mongoose.Types.ObjectId(),
+		name: req.body.name,
+		description: req.body.description,
+		price: req.body.price,
 	});
+	product
+		.save()
+		.then((result) => {
+			res.status(201).json({
+				message: 'Handling POST requests to /products',
+				createdProduct: result,
+			});
+		})
+		.catch((err) => {
+			res.status(500).json({
+				error: err,
+			});
+		});
 });
 
 module.exports = router;
