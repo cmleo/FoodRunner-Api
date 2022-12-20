2f40d782acaddc1a0f28768cb7c5a2a2fc642fdb
cmleo <leo.marinel13@yahoo.com>
2022-12-20 15:43:14 +0200

    deleted address from user schema

--- Api/Models/User.js
+++ Api/Models/User.js
@@ -2,12 +2,30 @@ const mongoose = require('mongoose');
 
 // Define schema and model for users
 const userSchema = new mongoose.Schema({
-	name: { type: String, required: true, unique: true },
-	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
-	phone: { type: String, required: true },
-	address: { type: String },
-	orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	phone: {
+		type: String,
+		required: true,
+	},
+	orders: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Order',
+		},
+	],
 });
 const User = mongoose.model('User', userSchema);
 
