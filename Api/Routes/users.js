4b70bf0fbfee2d12aef54d8adb7a8b2e5ac34714
cmleo <leo.marinel13@yahoo.com>
2022-12-21 13:15:56 +0200

    added delete user by id parameter route

--- Api/Routes/users.js
+++ Api/Routes/users.js
@@ -46,4 +46,21 @@ router.post('/signup', (req, res, next) => {
 		});
 });
 
+router.delete('/:userId', (req, res, next) => {
+	User.remove({ _id: req.params.userId })
+		.exec()
+		.then((result) => {
+			res.status(200).json({
+				message: 'User deleted',
+				result: result,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				error: err,
+			});
+		});
+});
+
 module.exports = router;
