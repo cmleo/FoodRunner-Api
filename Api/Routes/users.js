d0985289b6130966669eb4a4c81259a0449b1f13
cmleo <leo.marinel13@yahoo.com>
2022-12-21 12:23:22 +0200

    checked if the user already exists

--- Api/Routes/users.js
+++ Api/Routes/users.js
@@ -6,34 +6,44 @@ const bcrypt = require('bcrypt');
 const User = require('../Models/User');
 
 router.post('/signup', (req, res, next) => {
-	bcrypt.hash(req.body.password, 10, (err, hash) => {
-		if (err) {
-			return res.status(500).json({
-				error: err,
-			});
-		} else {
-			const user = new User({
-				_id: new mongoose.Types.ObjectId(),
-				name: req.body.name,
-				email: req.body.email,
-				password: hash,
-				phone: req.body.phone,
-			});
-			user
-				.save()
-				.then((result) => {
-					res.status(201).json({
-						message: 'User created succesfully',
-						CreatedUser: result,
-					});
-				})
-				.catch((err) => {
-					res.status(500).json({
-						error: err,
-					});
+	User.find({ email: req.body.email })
+		.exec()
+		.then((user) => {
+			if (user.length >= 1) {
+				return res.status(409).json({
+					message: 'User Already Exists',
 				});
-		}
-	});
+			} else {
+				bcrypt.hash(req.body.password, 10, (err, hash) => {
+					if (err) {
+						return res.status(500).json({
+							error: err,
+						});
+					} else {
+						const user = new User({
+							_id: new mongoose.Types.ObjectId(),
+							name: req.body.name,
+							email: req.body.email,
+							password: hash,
+							phone: req.body.phone,
+						});
+						user
+							.save()
+							.then((result) => {
+								res.status(201).json({
+									message: 'User created succesfully',
+									CreatedUser: result,
+								});
+							})
+							.catch((err) => {
+								res.status(500).json({
+									error: err,
+								});
+							});
+					}
+				});
+			}
+		});
 });
 
 module.exports = router;
