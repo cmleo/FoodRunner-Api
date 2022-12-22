ad9a71f96440ac44d945b5528a6b79164b5d316c
cmleo <leo.marinel13@yahoo.com>
2022-12-22 09:34:22 +0200

    replaced the mongodb string with the environment variable

--- Api/app.js
+++ Api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const productRoutes = require('./Routes/products');
 const userRoutes = require('./Routes/users');
