04c29be1f8f78cc18b382d73f8d74542be59aa27
cmleo <leo.marinel13@yahoo.com>
2023-03-02 12:42:57 +0200

    changed some properties in order schema

--- Api/Models/Order.js
+++ Api/Models/Order.js
@@ -2,18 +2,17 @@ const mongoose = require('mongoose');
 
 // Define schema and model for orders
 const orderSchema = new mongoose.Schema({
-	user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-	restaurant_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant' },
-	items: [
+	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	order: [
 		{
-			product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-			name: { type: String, required: true },
-			price: { type: Number, required: true },
+			productName: { type: String, required: true },
 			quantity: { type: Number, default: 1 },
+			price: { type: Number, required: true },
 		},
 	],
-	delivery_address: { type: String, required: true },
-	total_price: { type: Number, required: true },
+	deliveryAddress: { type: String, required: true },
+	totalPrice: { type: Number, required: true },
+	timestamp: { type: Date, default: Date.now },
 });
 const Order = mongoose.model('Order', orderSchema);
 
