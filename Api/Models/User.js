a691ad8af3087ffee6caea3b58dfab3ce037dfba
cmleo <leo.marinel13@yahoo.com>
2023-03-02 16:50:14 +0200

    deleted orders field from user schema

--- Api/Models/User.js
+++ Api/Models/User.js
@@ -22,12 +22,6 @@ const userSchema = new mongoose.Schema({
 		required: true,
 		unique: true,
 	},
-	orders: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Order',
-		},
-	],
 });
 const User = mongoose.model('User', userSchema);
 
