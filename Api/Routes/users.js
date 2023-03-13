2e3c6ab9e7e87f134ca0a29978cdd91b619bb958
cmleo <leo.marinel13@yahoo.com>
2023-03-13 21:41:48 +0200

    updated some result messages in user routes

--- Api/Routes/users.js
+++ Api/Routes/users.js
@@ -56,14 +56,14 @@ router.post('/login', (req, res, next) => {
 		.then((user) => {
 			if (!user) {
 				return res.status(401).json({
-					message: 'Auth failed',
+					message: "Email doesn't exist",
 				});
 			}
 			bcrypt.compare(req.body.password, user.password, (err, result) => {
 				if (err) {
 					console.log(err);
 					return res.status(401).json({
-						message: 'Auth failed',
+						message: 'Password is incorrect',
 					});
 				}
 
@@ -121,9 +121,9 @@ router.delete('/:userId', checkAuth, (req, res, next) => {
 	User.remove({ _id: userId })
 		.exec()
 		.then((result) => {
-			res.status(200).json({
-				message: 'User deleted',
-				result: result,
+			res.status(204).json({
+				message: 'User deleted successfully',
+				DeletedUser: result,
 			});
 		})
 		.catch((err) => {
@@ -168,6 +168,7 @@ router.patch('/', checkAuth, (req, res) => {
 		.then((result) => {
 			res.status(200).json({
 				message: 'User info updated',
+				UpdatedUser: result,
 			});
 		})
 		.catch((err) => {
