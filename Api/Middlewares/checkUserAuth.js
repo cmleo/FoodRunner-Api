06ad96cd72d06c2c356b0ad078641c65a2a1dd5c
cmleo <leo.marinel13@yahoo.com>
2023-03-14 17:13:10 +0200

    added new verification in checkUserAuth middleware

--- Api/Middlewares/checkUserAuth.js
+++ Api/Middlewares/checkUserAuth.js
@@ -4,6 +4,11 @@ module.exports = (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(' ')[1];
 		const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+		if (decoded.role !== 'user') {
+			throw new Error();
+		}
+
 		req.userData = decoded;
 		next();
 	} catch (error) {
